0396bf267da8dcd9f2c6d545f3ff68652fa34100
Min Kim <minmkim@gmail.com>
2018-07-07 01:47:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = { food: "test" }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  var newObject = object.assign({}, object)
+  newObject.key = value
+  return newObject
 }
