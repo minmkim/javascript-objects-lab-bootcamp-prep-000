04c47d847de050c1e52037e4f9222ecb8180f73f
Min Kim <minmkim@gmail.com>
2018-07-07 01:50:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObject
 }
 
-function destructivelyUpdateObjectiveWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
